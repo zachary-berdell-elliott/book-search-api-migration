64628b69511a9e048a57d7564b5e7944d4f1a072
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-03 13:30:44 -0700

    Query object in the resolvers has been built.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,20 @@ const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-
+    Query: {
+        users: async () => {
+            return User.find().populate('books');
+        },
+        user: async (parent, { username }) => {
+            return User.FindOne({ username }).populate('books');
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('books');
+              }
+              throw new AuthenticationError('You have to log in first');
+        }
+    }
 }
 
 module.exports = resolvers;
