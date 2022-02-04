d1485594b13457f62c68bde413c40d7c3b5e5324
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-03 17:39:58 -0700

    User variable written in the mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,33 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }`;
+
+export const ADD_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($userId: ID!, $bookId: ID!) {
+        saveBook(userId: $userId, bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks 
+        }
+    }`
