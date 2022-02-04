05d32bf2c42d09a711e6773fafe6c7132c858077
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-03 17:19:41 -0700

    Added the neccessary code to the app.js and added additional files that will be needed.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,6 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        users: async () => {
-            return User.find().populate('books');
-        },
-        user: async (parent, { username }) => {
-            return User.FindOne({ username }).populate('books');
-        },
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id }).populate('books');
