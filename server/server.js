3626c1ddab4c60a617038b749507c5e6d22a7560
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-05 16:08:52 -0700

    server.js updated to work with GraphQL.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +21,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`The API server is running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`GraphQL can be accessed at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
