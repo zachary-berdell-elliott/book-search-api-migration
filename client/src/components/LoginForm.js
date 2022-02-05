e3a4cfef7bebe3e0b3504cec19ca7107012b112a
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-05 16:46:08 -0700

    package.json's updated.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,7 +26,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await useMutation(LOGIN_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
