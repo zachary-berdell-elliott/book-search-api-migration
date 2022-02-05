daec8bb6edad3f447f0e9306042d87be83c28570
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-05 16:41:03 -0700

    SignupForm updated to work with GraphQL.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -28,7 +28,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await useMutation(ADD_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
