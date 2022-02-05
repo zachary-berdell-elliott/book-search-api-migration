dc347c284569f297b1549a037105f34de3709aff
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-05 16:25:11 -0700

    searchBooks.js updated to work with GraphQL.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -65,7 +66,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await useMutation(SAVE_BOOK);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
