0f5e74748eee599a22ee73f7a5ad9d404a3f34da
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-06 11:54:20 -0700

    Project now builds without errors in the process.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,6 +15,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -66,7 +68,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = useMutation(SAVE_BOOK);
+      const response = await saveBook(bookToSave, token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
