71aed4dd92d13508c4e12a72ec0d43b41146b4cb
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-07 11:47:25 -0700

    Debugged an issue with password being capitalized in the creatUser typeDef.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,7 +25,7 @@ const typeDefs = gql`
         me: User
     }
     type Mutation {
-        createUser(username: String!, email: String!, Password: String!): Auth
+        createUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
         saveBook(userId: ID!, bookId: ID!): User
         deleteBook(userId: ID!, bookId: ID!): User
