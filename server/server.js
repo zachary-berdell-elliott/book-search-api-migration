7ce02283a22bbfca6e8ec84bdafea4d62dd4cef7
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-15 11:28:20 -0700

    Troubleshooting heroku deployment issues.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
